5409ec23523de76b4ae1bd2f1a84ee96feb94905
unknown <idahosajoshua61@gmail.com>
2022-03-21 10:29:31 +0100

    fixed the special tile click function

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Coursera-Module-5-Solution
-Created this website to fufill Coursera HTML,CSS and JS for Web Developers Course 
-![front_look](https://user-images.githubusercontent.com/31415089/159168449-aa44763f-47de-4a3b-b9b6-ecc470bd93ad.png)
+# assignment-module-5
--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -1,16 +1,10 @@
 (function (global) {
-
-// Set up a namespace for our utility
 var ajaxUtils = {};
-
-
-// Returns an HTTP request object
 function getRequestObject() {
   if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
   else if (window.ActiveXObject) {
-    // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
   else {
@@ -18,9 +12,6 @@ function getRequestObject() {
     return(null); 
   }
 }
-
-
-// Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = 
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
@@ -31,24 +22,16 @@ ajaxUtils.sendGetRequest =
                        isJsonResponse); 
       };
     request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+    request.send(null); 
   };
-
-
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
 function handleResponse(request,
                         responseHandler,
                         isJsonResponse) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
-
-    // Default to isJsonResponse = true
     if (isJsonResponse == undefined) {
       isJsonResponse = true;
     }
-
     if (isJsonResponse) {
       responseHandler(JSON.parse(request.responseText));
     }
@@ -58,10 +41,6 @@ function handleResponse(request,
   }
 }
 
-
-// Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
-
-
 })(window);
 
